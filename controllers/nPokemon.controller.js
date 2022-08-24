44bb44965ca63171ec7c05f686692e37a3109b1b
Ignacio Romaní <ignacioromani@gmail.com>
2022-08-24 21:09:23 +0200

    se ve la foto del pokemon

--- controllers/nPokemon.controller.js
+++ controllers/nPokemon.controller.js
@@ -6,6 +6,13 @@ module.exports.list = (req, res, next) => {
     .getAllPokemon()
       .then((response) => {
         let pokemon = response.data.results;
-        res.render('npokemon/list', { pokemon: pokemon.slice(0, 151) });
+        const pokemonWithId = pokemon.map(item => {
+          return {
+            ...item,
+            id: item.url.slice(34, -1)
+          }
+        })
+        console.log(pokemonWithId);
+        res.render('npokemon/list', { pokemon: pokemonWithId.slice(0, 151)});
       })
 };
